8b9ea3b2b3dc251cababbf0d555010d87cbd0451
Faqih Syarif Ash-Shiddiq <146741872+ashfaqih@users.noreply.github.com>
2024-08-26 16:15:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ def resize_image(image_path, size=(300, 300)):
 def find_images_in_folder(folder):
     image_paths = []
     for root, dirs, files in os.walk(folder):
-        for file in files:
+        for file in sorted(files):
             if file.lower().endswith(('.png', '.jpg', '.jpeg')):
                 image_paths.append(os.path.join(root, file))
     return image_paths
